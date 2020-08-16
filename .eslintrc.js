29c72ffd1f37783bc82e2b7deab50f566e132803
kuba1pie <kuba1pie@gmail.com>
2020-08-17 00:43:17 +0200

    noconsole

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,9 +5,10 @@ module.exports = {
   },
   extends: ["plugin:vue/essential", "@vue/prettier", "@vue/typescript"],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-console": 0
+    //"no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     //"no-console": "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    //"no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
   },
   parserOptions: {
     parser: "@typescript-eslint/parser"
